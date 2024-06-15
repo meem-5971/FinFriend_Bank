76feda3aa721cce804fb3b19dcf1dad4224922cc
Khushnor Rahman Meem <khushnorrahmanmeem@gmail.com>
2024-06-15 15:29:11 +0600

    Changes done

--- finfriend/settings.py
+++ finfriend/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGIN = ['https://finfriend.onrender.com','https://*.127.0.0.1',]
+CSRF_TRUSTED_ORIGINS= ['https://finfriend.onrender.com','https://*.127.0.0.1',]
 
 # Application definition
 
